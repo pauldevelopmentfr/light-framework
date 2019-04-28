15c5ba5a18e0b7c874c46d55d1072ba31d277d10
Paul Development <paulsnnh@gmail.com>
2019-04-28 15:00:02 +0200

    Fix Autoloader bug due to the case sensitivity

--- app/core/Autoloader.php
+++ app/core/Autoloader.php
@@ -12,6 +12,12 @@ class Autoloader
         spl_autoload_register(function ($className) {
             $className = str_replace('\\', '/', $className);
 
+            if (preg_match('/core/i', $className)) {
+                $className = str_replace('App/Core', 'app/core', $className);
+            } elseif (preg_match('/local/i', $className)) {
+                $className = str_replace('App/Local', 'app/local', $className);
+            }
+
             $cwd = getcwd();
 
             $fileName = "{$cwd}/{$className}.php";
